6eb68652919d363e331354720f8a326ac8122617
Yuheng Zhang <2845024327@qq.com>
2022-06-06 15:04:33 +0800

    development environment

--- apps.py
+++ apps.py
@@ -70,4 +70,4 @@ def research1_zh():
 	return render_template('research_zh.html')
 
 if __name__ == '__main__':
-	serve(app, host='0.0.0.0', port=8000)
+	app.run(host='0.0.0.0', port=8000, debug=False)
