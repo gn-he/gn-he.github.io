7072825e244a085e0e80ef723199ebeda69d0a11
Yuheng Zhang <2845024327@qq.com>
2022-06-06 15:19:54 +0800

    adhoc

--- apps.py
+++ apps.py
@@ -70,4 +70,4 @@ def research1_zh():
 	return render_template('research_zh.html')
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000, debug=False)
+	app.run(host='0.0.0.0', port=8000, ssl_context='adhoc')
